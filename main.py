7e78212fee1665c4f857c875de3630e9d49e24f1
夏侯景明 <153259201+suoyinxcn@users.noreply.github.com>
2026-01-17 18:25:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import time
 import requests
 
-PROFILE_URL = os.getenv(
-    "GITHUB_PROFILE",
-    "https://github.com/1800797"
+CAMO_URL = os.getenv(
+    "CAMO_URL",
+    "https://camo.githubusercontent.com/758f27cbbe4be3c2b639357d9acc17d39d78b792b57f745902f0a12aea835dd7/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d786d6f72696e6f7269267374796c653d666f722d7468652d6261646765"
 )
 
 HEADERS = {
@@ -13,9 +13,10 @@ HEADERS = {
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/121.0.0.0 Safari/537.36"
     ),
-    "Accept": "text/html,application/xhtml+xml",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Connection": "keep-alive"
+    "Accept": "image/avif,image/webp,image/apng,image/*,*/*;q=0.8",
+    "Referer": "https://github.com/",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache"
 }
 
 session = requests.Session()
@@ -23,21 +24,16 @@ session.headers.update(HEADERS)
 
 while True:
     try:
-        r = session.get(PROFILE_URL, timeout=15)
-        print("PROFILE:", r.status_code)
+        r = session.get(CAMO_URL, timeout=10)
+        print("CAMO LOAD:", r.status_code, "bytes:", len(r.content))
 
-        # kalau sukses, "liat" bentar
-        if r.status_code == 200:
-            time.sleep(2)
-
-        # kalau GitHub nge-limit
         if r.status_code == 429:
-            print("RATE LIMIT – tidur 10 detik")
-            time.sleep(10)
+            print("RATE LIMIT – sleep 15s")
+            time.sleep(15)
 
     except Exception as e:
         print("ERROR:", e)
         time.sleep(5)
 
-    # delay kecil (WAJIB biar Railway aman)
-    time.sleep(1.5)
+    # delay kecil biar Railway gak kill
+    time.sleep(2)
